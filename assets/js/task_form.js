208ef8f398f6beec8335c41643a01b996b8fa946
Arianne Gallardo <augallardo@up.edu.ph>
2022-03-23 12:46:39 +0800

    - Add WYSIWYG editor for task details - Update alert info; add animation - Fix task/form timestamps behaviour - Fix task details in task card

--- assets/js/task_form.js
+++ assets/js/task_form.js
@@ -6,6 +6,42 @@ $(document).ready(function() {
     $("#task_deadline_minute").prop("disabled", deadline_disabled);
     $("#task_completed_on_hour").prop("disabled", completed_on_disabled);
     $("#task_completed_on_minute").prop("disabled", completed_on_disabled);
+
+    // $("#task_completed_on").triggerHandler("change");/
+    if($("#completed_on_checkbox").is(":checked")) {
+        var completed_on_str = $("#task_completed_on").val();
+        var completed_on_date = new Date(completed_on_str);
+
+        $("#task_completed_on_year").val(completed_on_date.getFullYear());
+        $("#task_completed_on_month").val(completed_on_date.getMonth() + 1);
+        $("#task_completed_on_day").val(completed_on_date.getDate());
+
+        // $("#task_completed_on_hour").triggerHandler("change");
+        var hour = $("#task_completed_on_hour");
+        $("#task_completed_on_hour_field").val(hour.val());
+
+        // $("#task_completed_on_minute").triggerHandler("change");
+        var minute = $("#task_completed_on_minute");
+        $("#task_completed_on_minute_field").val(minute.val());
+    }
+
+    if($("#deadline_checkbox_checkbox").is(":checked")) {
+        // $("#task_deadline").triggerHandler("change");
+        var deadline_str = $("#task_deadline").val();
+        var deadline_date = new Date(deadline_str);
+
+        $("#task_deadline_year").val(deadline_date.getFullYear());
+        $("#task_deadline_month").val(deadline_date.getMonth() + 1);
+        $("#task_deadline_day").val(deadline_date.getDate());
+
+        // $("#task_deadline_hour").triggerHandler("change");
+        var hour = $("#task_deadline_hour");
+        $("#task_deadline_hour_field").val(hour.val());
+
+        // $("#task_deadline_minute").triggerHandler("change");
+        var minute = $("#task_deadline_minute");
+        $("#task_deadline_minute_field").val(minute.val());
+    }
 });
 
 $(".disable-timestamp").change(function() {
@@ -67,14 +103,48 @@ $("#task_deadline").change(function() {
     $("#task_deadline_year").val(deadline_date.getFullYear());
     $("#task_deadline_month").val(deadline_date.getMonth() + 1);
     $("#task_deadline_day").val(deadline_date.getDate());
+
+    // $("#task_deadline_hour").triggerHandler("change");
+    var hour = $("#task_deadline_hour");
+    $("#task_deadline_hour_field").val(hour.val());
+
+    // $("#task_deadline_minute").triggerHandler("change");
+    var minute = $("#task_deadline_minute");
+    $("#task_deadline_minute_field").val(minute.val());
 });
 
 $("#task_deadline_hour").change(function() {
     $("#task_deadline_hour_field").val($(this).val());
+
+    // $("#task_deadline").triggerHandler("change");
+    var date = $("#task_deadline");
+    var deadline_str = date.val();
+    var deadline_date = new Date(deadline_str);
+
+    $("#task_deadline_year").val(deadline_date.getFullYear());
+    $("#task_deadline_month").val(deadline_date.getMonth() + 1);
+    $("#task_deadline_day").val(deadline_date.getDate());
+
+    // $("#task_deadline_minute").triggerHandler("change");
+    var minute = $("#task_deadline_minute");
+    $("#task_deadline_minute_field").val(minute.val());
 });
 
 $("#task_deadline_minute").change(function() {
     $("#task_deadline_minute_field").val($(this).val());
+
+    // $("#task_deadline").triggerHandler("change");
+    var date = $("#task_deadline");
+    var deadline_str = date.val();
+    var deadline_date = new Date(deadline_str);
+
+    $("#task_deadline_year").val(deadline_date.getFullYear());
+    $("#task_deadline_month").val(deadline_date.getMonth() + 1);
+    $("#task_deadline_day").val(deadline_date.getDate());
+
+    // $("#task_deadline_hour").triggerHandler("change");
+    var hour = $("#task_deadline_hour");
+    $("#task_deadline_hour_field").val(hour.val());
 });
 
 $("#task_completed_on").change(function() {
@@ -84,12 +154,55 @@ $("#task_completed_on").change(function() {
     $("#task_completed_on_year").val(completed_on_date.getFullYear());
     $("#task_completed_on_month").val(completed_on_date.getMonth() + 1);
     $("#task_completed_on_day").val(completed_on_date.getDate());
+
+    // $("#task_completed_on_hour").triggerHandler("change");
+    var hour = $("#task_completed_on_hour");
+    $("#task_completed_on_hour_field").val(hour.val());
+
+    // $("#task_completed_on_minute").triggerHandler("change");
+    var minute = $("#task_completed_on_minute");
+    $("#task_completed_on_minute_field").val(minute.val());
 });
 
 $("#task_completed_on_hour").change(function() {
     $("#task_completed_on_hour_field").val($(this).val());
+
+    // $("#task_completed_on").triggerHandler("change");
+    var date = $("#task_completed_on");
+    var completed_on_str = date.val();
+    var completed_on_date = new Date(completed_on_str);
+
+    $("#task_completed_on_year").val(completed_on_date.getFullYear());
+    $("#task_completed_on_month").val(completed_on_date.getMonth() + 1);
+    $("#task_completed_on_day").val(completed_on_date.getDate());
+
+    // $("#task_completed_on_minute").triggerHandler("change");
+    var minute = $("#task_completed_on_minute");
+    $("#task_completed_on_minute_field").val(minute.val());
 });
 
 $("#task_completed_on_minute").change(function() {
     $("#task_completed_on_minute_field").val($(this).val());
+
+    // $("#task_completed_on").triggerHandler("change");
+    var date = $("#task_completed_on");
+    var completed_on_str = date.val();
+    var completed_on_date = new Date(completed_on_str);
+
+    $("#task_completed_on_year").val(completed_on_date.getFullYear());
+    $("#task_completed_on_month").val(completed_on_date.getMonth() + 1);
+    $("#task_completed_on_day").val(completed_on_date.getDate());
+
+    // $("#task_completed_on_hour").triggerHandler("change");
+    var hour = $("#task_completed_on_hour");
+    $("#task_completed_on_hour_field").val(hour.val());
+});
+
+$(".task-form-container").submit(function(event) {
+    event.preventDefault();
+
+    var details = $(".ql-editor").html();
+    $("#details-textarea").text(details);
+
+    $(this).unbind("submit").submit();
 });
