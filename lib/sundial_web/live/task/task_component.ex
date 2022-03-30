f141d4d96aa49e453c8a0bbb479b64bd79e7c19f
Arianne Gallardo <augallardo@up.edu.ph>
2022-03-31 02:38:12 +0800

    TaskComponent code cleanup

--- lib/sundial_web/live/task/task_component.ex
+++ lib/sundial_web/live/task/task_component.ex
@@ -1,34 +1,20 @@
 defmodule SundialWeb.Live.Task.TaskComponent do
   use Phoenix.LiveComponent
-  import SundialWeb.TaskLive.Sections
-  import Phoenix.HTML
-  import Phoenix.HTML.Safe
 
-  # alias SundialWeb.TaskView
+  import SundialWeb.TaskLive.Sections
+
   alias Sundial.Progress.States
   alias Sundial.Tasks
 
-  # def render(assigns) do
-  #   TaskView.render("show.html", assigns)
-  # end
-
-  # def render(a,b) do
-  #   IO.inspect a
-
-  # end
-
   def mount(_params, _session, socket) do
-    # IO.inspect("inspectdb: in mount/3 of taskcomponent")
     {:ok, assign(socket, %{status: States.get()})}
   end
 
   def mount(_session, socket) do
-    # IO.inspect("inspectdb: in mount/2 of taskcomponent")
     {:ok, assign(socket, %{status: States.get()})}
   end
 
   def mount(socket) do
-    # IO.inspect("inspectdb: in mount/1 of taskcomponent")
     {:ok, assign(socket, %{status: States.get()})}
   end
 
@@ -47,45 +33,29 @@ defmodule SundialWeb.Live.Task.TaskComponent do
   end
 
   def update(assigns, socket) do
-    # IO.inspect "in task component update"
     {:ok,
       socket
       |> assign(assigns)}
   end
 
-  # Listen for status change from TaskComponent (index view)
   def handle_event("update_status", task_params, socket) do
-    # IO.inspect status_id %{"status" => status_id}
     task = Tasks.get_task!(socket.assigns.task.id)
 
     Map.put(task_params, "completed_on", NaiveDateTime.local_now)
 
     case Tasks.update_task(task, task_params) do
       {:ok, _task} ->
-        # {:noreply, assign(socket, %{task: Tasks.serialize_task(task)})}
-        # send_update(pid \\ self(), module, assigns)
-        # redirect(socket, arg2)
-        # socket = put_flash(socket, :info, "Status updated!")
-        # {:ok, assign(socket, %{new: "new"})}
         {:reply, socket, push_redirect(socket, to: "/")}
 
       {:error, %Ecto.Changeset{} = _changeset} ->
         put_flash(socket, :error, "Can't update status as of the moment. Please try again later.")
     end
-
-    # {:noreply, socket}
-    # {:reply, map, %Socket}
   end
 
-  # task_params: %{"before" => before, "after" => after}
+  # No longer used -- replaced by the drag&drop functionality
   def handle_event("update_position", params, socket) do
     task = Tasks.get_task!(socket.assigns.task.id)
     index = params["index"] |> String.to_integer()
-
-    # before -- position of the element before self, after -- position of the element after self
-    # IO.puts "in task component update position"
-
-    # assign moves
     moves = if params["down"], do: params["down"] |> String.to_integer, else: -(params["up"] |> String.to_integer)
 
     case Tasks.find_insert_position(task, index, moves) do
@@ -105,10 +75,5 @@ defmodule SundialWeb.Live.Task.TaskComponent do
       {:error, _} ->
         put_flash(socket, :error, "Can't reorder tasks as of the moment. Please try again later.")
     end
-    # if same column --> reorder
-      # call handle_event update_position? --> req : socket, params {down/up, index}
-
-    # else return error
-    # {:noreply, socket}
   end
 end
